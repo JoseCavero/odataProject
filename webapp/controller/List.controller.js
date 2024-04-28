b3336972153401d060bd760bd08da1731b3dad56
Jose Antonio Cavero <joseantoniocavero15@gmail.com>
2024-04-28 13:36:27 +0200

    Search filter on ProductID and Category fields

--- webapp/controller/List.controller.js
+++ webapp/controller/List.controller.js
@@ -1,15 +1,34 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter, FilterOperator, JSONModel) {
         "use strict";
 
         return Controller.extend("jose.cavero.odatap.odataproject.controller.List", {
             onInit: function () {
 
+            }, 
+            onSearch: function (oEvent) {
+                // Get data
+                var sQuery = oEvent.getParameter("query");
+				var oTable = this.getView().byId("listTable");
+				var oBinding = oTable.getBinding("items");
+
+                // Build filters
+				var aFilter = [];
+				aFilter.push(new Filter(
+					[new Filter("ProductID", FilterOperator.Contains, sQuery), new Filter("Category", FilterOperator.Contains, sQuery)],
+					false
+				));
+                // Apply filters
+				oBinding.filter(aFilter);
             }
-        });
     });
+    }
+);
