9bf3bb0254b50993eef0b0866b2287cd25b33015
Jose Antonio Cavero <joseantoniocavero15@gmail.com>
2024-04-28 12:02:22 +0200

    Initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["jose/cavero/odatap/odataproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"josecaveroodatap/odataproject/test/unit/controller/List.controller"
+], function () {
+	"use strict";
+});
